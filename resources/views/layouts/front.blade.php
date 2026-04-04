34ec3af7c0166c5b12deb73dff99f9aa60b1646c
vw-uday <verloop.dev4@gmail.com>
2026-04-04 18:18:23 +0530

    remove google code

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -3,10 +3,6 @@
 @include('partials.front.head')
 
 <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TN4W7WVS"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
 
     <!-- PAGE CONTENT -->
     <div id="page" class="page font--poppins">
